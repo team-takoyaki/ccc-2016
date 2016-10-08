b28191f4b02c551eed489c223946ef5948790099
Takashi-kun <takashi.0628.honda@gmail.com>
2016-10-08 11:51:14 +0900

    add pdo

--- server/index.php
+++ server/index.php
@@ -2,9 +2,22 @@
 
 require_once 'lib/limonade.php';
 
+function get_pdo() {
+    $pdo = new PDO(
+        'mysql:host=localhost;dbname=db_katte_honda;charset=utf8',
+        'katte',
+        'kattepass',
+        array(PDO::ATTR_EMULATE_PREPARES => false)
+    );
+    return $pdo;
+}
+
 dispatch('/', 'hello');
 function hello() {
-    return 'Hello world!';
+    $pdo = get_pdo();
+    $stmt = $pdo->query("SELECT * FROM max_mention_count_by_grade");
+    $count = $stmt->rowCount();
+    return 'Hello world! honda' . $count . 'h';
 }
 
 run();
